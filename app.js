d805d2100562e52ffe6ec55f642bd563748f9dfc
Mohammad Javad Gorgani <mohammadjavadgorganii.ce@gmail.com>
2025-11-15 15:52:19 +0330

    update README.md

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const initDatabase = require("./src/config/model.init");
 async function main() {
   await initDatabase();
   require("./src/bot/index");
-  console.log("run:", "https://web.bale.ai/chat?uid=1583151881");
+  console.log("run:", "https://web.bale.ai/chat?uid=");
 }
 main();
